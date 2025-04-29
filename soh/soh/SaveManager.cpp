8bde2aabb34cc8b6ba4e8781407cf5732cb92fb8
Schicksal88 <schicksal88@web.de>
2025-04-29 21:22:14 +0200

    German translation cleanup
    - German ice trap names and code for it to show up. Changed the order of translations to english/french/german to make it consistent with item_list.cpp.
    - German shops as those are now complete.
    - German translation for ingame fileselect rando menu.
    - Added function to mark an item as plural. This is necessary to have proper grammar in most german sentences. If an item has a € at the end of it and the sentence it get's inserted into has |singular|plural| forms defined, the singular part get's removed.
    - Added the plural markers & |singular|plural| parts to the entire german script where it was necessary.
    - Fixed grammar, typos, inconsistencies and now everything uses official Zelda-game names.
    - Fixed vanilla/master quest map text

--- soh/soh/SaveManager.cpp
+++ soh/soh/SaveManager.cpp
@@ -169,6 +169,7 @@ void SaveManager::LoadRandomizerVersion1() {
                 randoContext->overrides[location].SetLocation(location);
                 SaveManager::Instance->LoadData("fakeRgID", randoContext->overrides[location].RefLooksLike());
                 SaveManager::Instance->LoadData("trickName", randoContext->overrides[location].GetTrickName().english);
+                SaveManager::Instance->LoadData("trickName", randoContext->overrides[location].GetTrickName().german);
                 SaveManager::Instance->LoadData("trickName", randoContext->overrides[location].GetTrickName().french);
             }
         });
@@ -257,6 +258,8 @@ void SaveManager::LoadRandomizerVersion2() {
                     Rando::ItemOverride(static_cast<RandomizerCheck>(i), rg);
                 SaveManager::Instance->LoadData(
                     "trickName", randoContext->overrides[static_cast<RandomizerCheck>(i)].GetTrickName().english);
+                SaveManager::Instance->LoadData(
+                    "trickName", randoContext->overrides[static_cast<RandomizerCheck>(i)].GetTrickName().german);
                 SaveManager::Instance->LoadData(
                     "trickName", randoContext->overrides[static_cast<RandomizerCheck>(i)].GetTrickName().french);
             }
@@ -396,6 +399,7 @@ void SaveManager::LoadRandomizerVersion3() {
                                                 Rando::ItemOverride(static_cast<RandomizerCheck>(i), rg));
                 SaveManager::Instance->LoadStruct("trickName", [&]() {
                     SaveManager::Instance->LoadData("english", randoContext->GetItemOverride(i).GetTrickName().english);
+                    SaveManager::Instance->LoadData("german", randoContext->GetItemOverride(i).GetTrickName().german);
                     SaveManager::Instance->LoadData("french", randoContext->GetItemOverride(i).GetTrickName().french);
                 });
             }
@@ -493,9 +497,10 @@ void SaveManager::SaveRandomizer(SaveContext* saveContext, int sectionID, bool f
                 SaveManager::Instance->SaveStruct("trickName", [&]() {
                     SaveManager::Instance->SaveData("english",
                                                     randoContext->GetItemOverride(i).GetTrickName().GetEnglish());
+                    SaveManager::Instance->SaveData("german",
+                                                    randoContext->GetItemOverride(i).GetTrickName().GetGerman());
                     SaveManager::Instance->SaveData("french",
                                                     randoContext->GetItemOverride(i).GetTrickName().GetFrench());
-                    // TODO: German (trick names don't have german translations yet)
                 });
             }
             if (randoContext->GetItemLocation(i)->HasCustomPrice()) {
